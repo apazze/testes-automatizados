5ea16f21e6d55888585bb2b010949a3de2d44830
Alisson Pazze <alisson_pazze@terceiros.sicredi.com.br>
2023-01-31 09:22:17 -0300

    Ajuste no teste da controller

--- src/test/java/com/example/testesautomatizados/controller/CarroControllerTest.java
+++ src/test/java/com/example/testesautomatizados/controller/CarroControllerTest.java
@@ -10,9 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.IOException;
-
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,6 +24,9 @@ class CarroControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     void deveCadastrarUmCarro() throws Exception {
 
@@ -33,18 +34,15 @@ class CarroControllerTest {
 
         mvc.perform(post("/cadastrar")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(toJson(carro)))
+                        .content(objectMapper.writeValueAsString(carro)))
                 .andExpect(status().isOk());
 
         verify(carroService).inserir(Carro.builder().build());
     }
 
-    private String toJson(Object object) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(NON_NULL);
-        mapper.findAndRegisterModules();
-
-        return mapper.writeValueAsString(object);
+    @Test
+    void notNull(){
+        assertNotNull(carroService);
     }
     //20230130 - Necessario @Getter na model para fazer o mapper p/ Json e EqualsAndHashCode para o verify
     //WebMvcTest para injetar a dependência no MockMvc
